4047a703f59d3c577f2932a1a312eb13a5f1063c
Michał Balas <m.balas1504@gmail.com>
2019-03-28 22:07:26 +0100

    Dodanie analizatora skłani

---
+++ src/args_analyzer.h
@@ -0,0 +1,8 @@
+#ifndef ELOHIM_JIMP2_ARGS_ANALYZER_H
+#define ELOHIM_JIMP2_ARGS_ANALYZER_H
+
+#include "game_config.c"
+
+config analyze_args (int, char**, config);
+
+#endif //ELOHIM_JIMP2_ARGS_ANALYZER_H
