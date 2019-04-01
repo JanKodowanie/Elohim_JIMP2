7be471b94e065b00a379fd130bdeec70c0c7d096
JanKodowanie <48965206+JanKodowanie@users.noreply.github.com>
2019-04-02 01:39:34 +0200

    Dodanie modułu do odczytu/zapisu txt

---
+++ src/txtFile_mngr.h
@@ -0,0 +1,11 @@
+#ifndef _FILE_MANAGER_
+#define _FILE_MANAGER_
+#include "board_storage.h"
+#include "game_config.h"
+#include "error_handler.h"
+
+void read_file(char*, board, char);
+void write_to_file(char*, board);
+
+
+#endif // _FILE_MANAGER_
