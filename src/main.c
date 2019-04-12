102df13ed0a1c3d4ff3506cd55947a7b590d8223
JanKodowanie <48965206+JanKodowanie@users.noreply.github.com>
2019-04-13 00:05:43 +0300

    Aktualizacja main.c
    Wykorzystanie funkcji free_heap do zwalniania pamięci.

--- src/main.c
+++ src/main.c
@@ -9,16 +9,30 @@
 #include "grid_randomizer.h"
 #include "game_of_life.h"
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
 
-    config cfg = analyze_args(argc, argv);
+    config cfg = malloc(sizeof(*cfg));
     board cells = malloc(sizeof(*cells));
 
-    if(cfg->random[0]==0||cfg->random[1]==0){
-        read_file(cfg->input, cells, cfg);
-	free(cfg->input);
+    int err_indicator = analyze_args(argc, argv, cfg);
+    if(err_indicator!=0)
+    {
+        free_heap(cfg, cells);
+        return EXIT_FAILURE;
     }
-    else{
+
+    if(cfg->random[0]==0||cfg->random[1]==0)
+    {
+        err_indicator = read_file(cfg->input, cells, cfg->defchar);
+        if(err_indicator!=0)
+        {
+            free_heap(cfg, cells);
+            return EXIT_FAILURE;
+        }
+    }
+    else
+    {
         cells->rows = cfg->random[0];
         cells->columns = cfg->random[1];
 
@@ -27,39 +41,30 @@ int main(int argc, char **argv){
     }
 
     transform_cells(cells, cfg);
+    printf("Cell transformation executed successfully!\n");
+
 
     if(cfg->backup == 1)
         write_to_file(cfg->output, cells);
-    else {
-	if (cells->rows < 20 && cells->columns < 20) {
-		printf("%i-th generation:\n", cfg->ngen);
-    		for (int i=0; i<cells->rows; i++) {
-			for (int j=0; j<cells->columns; j++)
-				printf("%c ", cells->matrix[i][j]);
-			printf("\n");
-		}
-	}
+    else if (cells->rows < 20 && cells->columns < 20)
+    {
+        printf("%i-th generation:\n", cfg->ngen);
+        for (int i=0; i<cells->rows; i++)
+        {
+            for (int j=0; j<cells->columns; j++)
+                printf("%c ", cells->matrix[i][j]);
+            printf("\n");
+        }
     }
-    if (cfg->output != NULL) {
-	   printf("Output files created in directory %s\n", cfg->output);
-	   free(cfg->output);
+    else
+    {
+        printf("Matrix too big to write to stdout. \n");
     }
+    if (cfg->output != NULL)
+        printf("Output files created in directory %s\n", cfg->output);
 
-    free(cfg);
-
-   for(int i=0; i<cells->rows; i++){
-       free(cells->matrix[i]);
-   }
-   free(cells->matrix);
-   free(cells);
-
+    free_heap(cfg, cells);
 
     return EXIT_SUCCESS;
 
-
-
-
-
 }
-
-
