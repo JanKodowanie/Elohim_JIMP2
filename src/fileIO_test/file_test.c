c39c74d0dce7ec5c8e304266b85926ff5db04a02
JanKodowanie <48965206+JanKodowanie@users.noreply.github.com>
2019-04-13 22:56:26 +0300

    fileIO_test - poprawa modułów

--- src/fileIO_test/file_test.c
+++ src/fileIO_test/file_test.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include "board_storage.h"
 #include "txtFile_mngr.h"
 
 
 int main(int argc, char **argv){
 
-    if(argc!=4){
-        printf("Podaj 3 argumenty: input, output, defchar\n");
+    if(argc!=3){
+        printf("Launch with 2 arguments: input, defchar\n");
         return EXIT_FAILURE;
     }
 
     char* input = argv[1];
-    char* output = argv[2];
-    char  defchar = argv[3][0];
+    char  defchar = argv[2][0];
+
+    char* output = "output";
+    mkdir(output, 0777);
 
     board test = malloc(sizeof(*test));
 
-    read_file(input, test, defchar);
+    int n = read_file(input, test, defchar);
 
-
+    if(n==0){
 
     for(int i=0; i<test->rows; i++){
 
@@ -34,15 +38,18 @@ int main(int argc, char **argv){
     write_to_file(output, test);
 
     for(int i=0; i<test->rows; i++)
-        free(test->matrix[i]);
+    free(test->matrix[i]);
 
     free(test->matrix);
     free(test);
-
-
+    }
+    else{
+        printf("Errors encountered!\n");
+        free(test);
+        return EXIT_FAILURE;
+    }
 
 
     return EXIT_SUCCESS;
 
 }
-
