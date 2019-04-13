89f3c2257f411da84eb2f41d89c4ed3fbd276c0d
JanKodowanie <48965206+JanKodowanie@users.noreply.github.com>
2019-04-13 22:57:54 +0300

    randomizer_test.c - aktualizacja

--- src/randomizer_test/randomizer_test.c
+++ src/randomizer_test/randomizer_test.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "board_storage.h"
 #include "grid_randomizer.h"
 int main(int argc, char **argv){
 
-    if(argc=4){
+    if(argc=4 && atoi(argv[1])>0 && atoi(argv[2])>0 && strlen(argv[3])==1 ){
     board test = malloc(sizeof(*test));
     test->rows = atoi(argv[1]);
     test->columns = atoi(argv[2]);
     char defchar = argv[3][0];
-    grid_randomizer(test, defchar);
+    fill_random(test, defchar);
 
     for(int i=0; i<test->rows; i++){
 
@@ -25,11 +26,11 @@ int main(int argc, char **argv){
             free(test->matrix[i]);
     free(test->matrix);
     free(test);
-    return 0;
+    return EXIT_SUCCESS;
     }
     else{
-    printf("Podaj 3 argumenty - row, col, znak\n");
-    return 1;
+    printf("Launch with 3 arguments - rows, columns, defchar\n");
+    return EXIT_FAILURE;
     }
 
 
